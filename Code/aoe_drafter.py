c972c15b2d192c193f7b0decfba0b3785f966b12
RafalVrunes <rafalek865@wp.pl>
2026-02-04 16:54:05 +0100

    some changes

--- Code/aoe_drafter.py
+++ Code/aoe_drafter.py
@@ -22,7 +22,7 @@ class FormatsCombobox(ttk.Combobox):
         self.formats_box.bind("<<ComboboxSelected>>", self.get_selected_key)
         self.formats_box.set("Select game format")
         # self.formats_box.pack(side=tk.TOP, anchor="e", padx=140, pady=20)
-        self.formats_box.grid(row=1, column=0, sticky='w', padx=1000, pady=25)
+        self.formats_box.grid(row=0, column=1, sticky="ne", padx=50, pady=85)
     
     def get_selected_key(self, event=None) -> str:
         self.key = self.formats_box.get()
@@ -36,18 +36,20 @@ class AoEApplication(tk.Tk):
         super().__init__()
         self.title("TEST")
         self.geometry("1340x650")
-        self.resizable(False, False)
+        # self.resizable(False, False)
         self.tabControl = ttk.Notebook(self)
+        self.tabControl.pack(expand=True, fill='both')
         self.tab1 = ttk.Frame(self.tabControl)
         self.tab2 = ttk.Frame(self.tabControl)
         self.tab3 = ttk.Frame(self.tabControl)
         self.tab4 = ttk.Frame(self.tabControl)
 
-        self.initialize_pages()
         
         self.page_1()
         self.pages_desc()
         self.page_2()
+        self.initialize_pages()
+
         self.maps_json: str = ""
         self.maps_json_path: str = ""
         self.game_format: str = ""
@@ -58,15 +60,17 @@ class AoEApplication(tk.Tk):
 
 
     def page_1(self) -> None:
-        self.top_frame = tk.Frame(self.tab1, width=0, height=0, bg='lightblue')
-        self.top_frame.grid(row=0, column=0, padx=0, pady=0)
-        # self.bottom_frame = tk.Frame(self.tab1, width=0, height=0, bg='lightblue')
+        # self.top_frame = tk.Frame(self.tab1, width=0, height=0, bg='lightblue')
+        # self.top_frame.grid(row=0, column=0, padx=0, pady=0)
+        # self.tabControl.add(self.bottom_frame, text="Page 3")
+        button_set_name = ttk.Button(self.tab1, text="Set name", command=self.set_show_name).grid(row=0, column=1, sticky="ne", padx=130, pady=25)
+        button_save = ttk.Button(self.tab1, text="Save", command=self.set_show_name).grid(row=0, column=1, sticky="ne", padx=240, pady=85)
+        button2 = ttk.Button(self.tab1, text="Load map file", command=self.load_maps_file).grid(row=0, column=0, sticky="nw", padx=50, pady=100)
+        game_format = FormatsCombobox(self.tab1)
+        # self.bottom_frame = tk.Frame(self.tab1, width=10, height=10, bg='green')
         # self.bottom_frame.grid(row=1, column=0, padx=0, pady=0)
-        button_set_name = ttk.Button(self.top_frame, text="Set name", command=self.set_show_name).grid(row=0, column=0, sticky="e", padx=1000, pady=25)
-        # button_set_name = ttk.Button(self.bottom_frame, text="Set name bottom frame", command=None).grid(row=0, column=0, sticky="e", padx=0, pady=0)
-        button_save = ttk.Button(self.top_frame, text="Save", command=self.set_show_name).grid(row=1, column=0, sticky="w", padx=900, pady=25)
-        button2 = ttk.Button(self.top_frame, text="Load map file", command=self.load_maps_file).grid(row=1, column=0, sticky='w', padx=50, pady=25)
-        game_format = FormatsCombobox(self.top_frame)
+        # button_bottom_frame = ttk.Button(self.bottom_frame, text="Set name bottom frame", command=None).grid(row=0, column=0, sticky="w", padx=1040, pady=220)
+
         
     
     def page_2(self) -> None:
@@ -74,7 +78,7 @@ class AoEApplication(tk.Tk):
         # return label_map_pool2
     
     def pages_desc(self) -> None:
-        label_map_pool = ttk.Label(self.top_frame, text="Map pool", font=("Times New Roman", 22)).grid(row=0, column=0, sticky='w', padx=50, pady=25)
+        label_map_pool = ttk.Label(self.tab1, text="Map pool", font=("Times New Roman", 22)).grid(row=0, column=0, sticky='nw', padx=50, pady=25)
         label_best_civs = ttk.Label(self.tab2, text="Best civs per map", font=("Times New Roman", 22)).pack(side=tk.TOP, anchor="w", padx=50, pady=25)
         label_civs_draft = ttk.Label(self.tab3, text="Civs draft", font=("Times New Roman", 22)).pack(side=tk.TOP, anchor="w", padx=50, pady=25)
         label_game_draft = ttk.Label(self.tab4, text="Game draft", font=("Times New Roman", 22)).pack(side=tk.TOP, anchor="w", padx=50, pady=25)
@@ -93,28 +97,35 @@ class AoEApplication(tk.Tk):
 
 
 
-    def recreate_page(self) -> None:
+    def recreate_page(self, tab: ttk.Frame) -> None:
         """ needs testing """
-        print(len(self.tab2.pack_slaves()), self.tab2.pack_slaves())
-        xd = [self.tab2.pack_slaves()[0].destroy()]
+        # for item in tab.grid_slaves():
+        #     print(item)
+        tab.grid_slaves()[5].destroy()
 
     def initialize_pages(self) -> None:
-        self.tabControl.add(self.tab1, text="Page 1")
-        self.tabControl.add(self.tab2, text="Page 2")
-        self.tabControl.add(self.tab3, text="Page 3")
-        self.tabControl.add(self.tab4, text="Page 4")
+        self.tab1.pack(expand=True, fill='both')
         self.tab1.columnconfigure(0, weight=1)
         self.tab1.columnconfigure(1, weight=1)
         self.tab1.rowconfigure(0, weight=1)
         self.tab1.rowconfigure(1, weight=1)
-        self.tabControl.grid(row=0, column=0)
+        # self.tab1.columnconfigure(2, weight=1)
+        # self.tab1.columnconfigure(3, weight=1)
+        # self.tab1.rowconfigure(2, weight=1)
+        # self.tab1.rowconfigure(3, weight=1)
+        self.tabControl.add(self.tab1, text="Page 1")
+        self.tabControl.add(self.tab2, text="Page 2")
+        self.tabControl.add(self.tab3, text="Page 3")
+        self.tabControl.add(self.tab4, text="Page 4")
+        # self.tabControl.grid(row=0, column=0)
 
     def set_show_name(self) -> None:
         name = askstring("Input", "Enter your name")
 
         if len(name) > 0: 
-                label = ttk.Label(self.top_frame, text=f"Player: {name}",font=("Times New Roman", 22))
-                label.grid(row=0, column=0, padx=1000, pady=25, sticky="nsew")
+                label = ttk.Label(self.tab1, text=f"Player: {name}",font=("Times New Roman", 22))
+                label.grid(row=0, column=1, padx=50, pady=25, sticky="ne")
+                self.recreate_page(self.tab1)
 
     def load_maps_file(self) -> None:
         filename = askopenfile()
@@ -151,30 +162,6 @@ class AoEApplication(tk.Tk):
     #     xd = tk.Label(text="Test2", width=170, height=45)
     #     xd.pack()
 
-
-
-# class InputForm(ttk.Frame):
-#     def __init__(self, parent):
-#         super().__init__(parent)
-
-#         self.columnconfigure(0, weight=1)
-#         self.rowconfigure(1, weight=1)
-
-#         self.entry = ttk.Entry(self)
-#         self.entry.grid(row=0, column=0, sticky="ew")
-#         self.entry.bind("<Return>", self.add_to_list)
-
-#         self.entry_btn = ttk.Button(self, text="Add", command=self.add_to_list)
-#         self.entry_btn.grid(row=0, column=1)
-
-#         self.text_list = tk.Listbox(self)
-#         self.text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
-    
-#     def add_to_list(self, event=None):
-#         text = self.entry.get()
-#         if text:
-#             self.text_list.insert(tk.END, text)
-#             self.entry.delete(0, tk.END)
 
 
 
